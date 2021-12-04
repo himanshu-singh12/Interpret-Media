f10183a6d9f18a5620ec93a673ead4bd70005b79
Himanshu singh <66276348+himanshu-singh12@users.noreply.github.com>
2021-12-05 01:07:59 +0530

    WP2Static CREATE wp-content/uploads/custom-css-js/108.js

---
+++ wp-content/uploads/custom-css-js/108.js
@@ -0,0 +1,11 @@
+<!-- start Simple Custom CSS and JS -->
+<script>const items = document.querySelectorAll(".accordion a");
+ 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));</script>
+
+<!-- end Simple Custom CSS and JS -->
